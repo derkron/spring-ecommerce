af2a6bc78bcbd33b316a762a04f860b970727b79
cigomez <crisgoma@suramericana.com.co>
2023-11-23 12:02:13 -0500

    implementacion para guardar la imagen del producto

--- src/main/java/com/curso/ecommerce/contoller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/contoller/ProductoController.java
@@ -3,60 +3,83 @@ package com.curso.ecommerce.contoller;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.ProductoService;
+import com.curso.ecommerce.service.UploadFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
-    private final Logger LOGGER= LoggerFactory.getLogger(ProductoController.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
     @Autowired
     private ProductoService productoService;
+    @Autowired
+    private UploadFileService upload;
+
     @GetMapping("")
-    public String show(Model model){
-        model.addAttribute("productos",productoService.findAll());
+    public String show(Model model) {
+        model.addAttribute("productos", productoService.findAll());
 
         return "productos/show";
     }
+
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "productos/create";
     }
+
     @PostMapping("/save")
-    public String save(Producto producto){
-        LOGGER.info("Este es el objeto producto {}",producto);
-        Usuario usuario = new Usuario(1,"","","","","", "","");
+    public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
+        LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario usuario = new Usuario(1, "", "", "", "", "", "", "");
         producto.setUsuario(usuario);
+
+        //imagen
+        if (producto.getId() == null){// cuando se crea un producto por primera vez
+          String nombreImagen = upload.saveImage(file);
+          producto.setImagen(nombreImagen);
+        }else{
+            if (file.isEmpty()){// cuando se edita el producto pero no se cambia la imagen
+                Producto product = new Producto();
+                product = productoService.get(producto.getId()).get();
+                producto.setImagen(product.getImagen());
+            }else{
+                String nombreImagen = upload.saveImage(file);
+                producto.setImagen(nombreImagen);
+            }
+        }
         productoService.save(producto);
         return "redirect:/productos";
     }
+
     @GetMapping("/edit/{id}")
-    public  String edit(@PathVariable Integer id, Model model){
+    public String edit(@PathVariable Integer id, Model model) {
         Producto producto = new Producto();
-        Optional<Producto> optionalProducto=productoService.get(id);
+        Optional<Producto> optionalProducto = productoService.get(id);
         producto = optionalProducto.get();
 
-        LOGGER.info("Detalles prodructo: {}",producto);
-        model.addAttribute("producto",producto);
+        LOGGER.info("Detalles prodructo: {}", producto);
+        model.addAttribute("producto", producto);
 
         return "productos/edit";
     }
+
     @PostMapping("/update")
-    public String update(Producto producto){
+    public String update(Producto producto) {
         productoService.update(producto);
         return "redirect:/productos";
     }
+
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id){
+    public String delete(@PathVariable Integer id) {
         productoService.delete(id);
 
         return "redirect:/productos";
